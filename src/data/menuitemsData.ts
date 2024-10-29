871158e9cbc396e2230b60a712cf60a10e5d1592
Diego Inga <137432829+DiegoInga@users.noreply.github.com>
2024-10-28 23:59:31 -0500

    0.0.0.2

---
+++ src/data/menuitemsData.ts
@@ -0,0 +1,18 @@
+import type { MenuItems } from "src/types/types"
+
+const menuItems:MenuItems[] = [
+    {
+        title: "Inicio",
+        url: "/",
+    },
+    {
+        title: "Servicios",
+        url: "/src/pages/servicios.astro",
+    },
+    {
+        title: "Sobre Nostros",
+        url: "/src/pages/nosotros.astro",
+    }
+]
+
+export default menuItems;
