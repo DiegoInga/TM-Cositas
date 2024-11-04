35ee9ecb55ab0f78f65064e2c9c51d245b971a5b
Diego Inga <137432829+DiegoInga@users.noreply.github.com>
2024-11-04 01:22:14 -0500

    0.0.0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,13 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
 // https://astro.build/config
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon()
+  ]
 });
